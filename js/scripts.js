b8cc1eb94e91d6d6b9dd998f6f6d1c376d8e3b24
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-07-25 20:39:23 +0200

    Esercizio completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,69 @@
 const startButton = document.getElementById('start-button');
 
 startButton.addEventListener ('click', function (){
+
     const gridContainer = document.getElementById('grid-container');
+
+    const diffLevels = document.getElementById('levels');
+    console.log(diffLevels.value);
+
+    let numberCells = 100;
+    
+    if (diffLevels.value == '2') {
+        numberCells = 81;
+    }
+    else if (diffLevels.value == '3') {
+        numberCells = 49;
+    }
+
+    /* Sintassi alternativa
+    
+    switch (diffLevels.value) {
+        case '2':
+            numberCells = 81;
+            break;
+
+        case '3':
+            numberCells = 49;
+            break;
+
+        default:
+            numberCells = 100;
+            break;
+    }*/
+
+    gridContainer.innerHTML = '';
 
     for (let i = 1; i <= 100; i++) {
 
         const newCell = document.createElement('div');
         newCell.innerHTML = i;
-        gridContainer.append(newCell);
+
+        /* Sintassi alternativa*/
+    
+        /*switch (numberCells) {
+            case 81:
+                newCell.classList.add('medium');
+                break;
+
+            case 49:
+                newCell.classList.add('hard');
+                break;
+
+            default:
+                newCell.classList.add('easy');
+                break;
+        }*/
+
+        if (numberCells == 100){
+            newCell.classList.add('easy');
+        }
+        else if (numberCells == 81){
+            newCell.classList.add('medium');
+        }
+        else if (numberCells == 49){
+            newCell.classList.add('hard');
+        }
 
         newCell.addEventListener ('click', function (){
             //console.log('this', this, typeof this);
@@ -15,5 +71,7 @@ startButton.addEventListener ('click', function (){
             this.classList.add('click-card');
             console.log(i);
         })
+
+        gridContainer.append(newCell);
     }
 })
