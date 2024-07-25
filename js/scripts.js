b71e2e23cb78a9a048ccc3228c4655f07be8f7b4
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-07-25 17:43:13 +0200

    Finito esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-console.log('JS Collegato')
+const startButton = document.getElementById('start-button');
+
+startButton.addEventListener ('click', function (){
+    const gridContainer = document.getElementById('grid-container');
+
+    for (let i = 1; i <= 100; i++) {
+
+        const newCell = document.createElement('div');
+        newCell.innerHTML = i;
+        gridContainer.append(newCell);
+
+        newCell.addEventListener ('click', function (){
+            //console.log('this', this, typeof this);
+
+            this.classList.add('click-card');
+            console.log(i);
+        })
+    }
+})
